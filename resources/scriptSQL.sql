4b88f0513898e9c25d8819ce8714e1379f47f545
Guilherme Ayala Cysne <134463225+GuilhermeAyala@users.noreply.github.com>
2026-04-30 00:45:57 -0300

    verificação SQL - ver se o database já existe

--- resources/scriptSQL.sql
+++ resources/scriptSQL.sql
@@ -12,3 +12,21 @@ CREATE TABLE contatos (
     id_usuario INT NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
 );
+
+CREATE DATABASE IF NOT EXISTS agenda_central;
+USE agenda_central;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    login VARCHAR(50) UNIQUE NOT NULL,
+    senha VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS contatos (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(40),
+    telefone VARCHAR(20),
+    dataCadastro DATE,
+    id_usuario INT NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+);
